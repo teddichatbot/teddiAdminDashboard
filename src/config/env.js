397c2741145d49c20a2e272128966b4746e73a69
subhankarCN <subhankar.ray@capitalnumbers.com>
2020-10-19 18:39:41 +0530

    modify in gitignore file

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,5 @@
-console.log("SERVER_ENV",process.env.VUE_APP_SERVER_ENV)
 const VUE_APP_SERVER_ENV = process.env.VUE_APP_SERVER_ENV? process.env.VUE_APP_SERVER_ENV : 'DEV';
-console.log("VUE_APP_SERVER_ENV",VUE_APP_SERVER_ENV)
+// console.log("VUE_APP_SERVER_ENV",VUE_APP_SERVER_ENV)
 const BASE_URL = (VUE_APP_SERVER_ENV == 'DEV')?'https://teddinodeapp.azurewebsites.net/':'https://teddibackend.azurewebsites.net/';
 // console.log("BASE_URL",BASE_URL)
 const config = {
